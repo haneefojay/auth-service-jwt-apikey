af98df289ecf7d5a63078c4ca651bc99d302e0bd
Ojutalayo Haneef abiola <biolahaneef@gmail.com>
2025-12-08 00:52:19 +0100

    increase access token expiry to 1 hour

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     TESTING: bool = False  # Set to True to disable rate limiting
     
     class Config:
